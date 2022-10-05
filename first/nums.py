fc5946df1f05e2e8459cf754dada88dfa80b6f00
Kirill Treskinskiy <jdevrus@gmail.com>
2022-10-05 14:21:29 +0400

    add def minus

--- first/nums.py
+++ first/nums.py
@@ -2,5 +2,13 @@ def show_sum(a, b: int) -> int:
     return a + b
 
 
+text = 'some txt'
+
+
+def show_minus(a, b: int) -> int:
+    return a - b
+
+
 if __name__ == '__main__':
     print(show_sum(2, 3))
+    print(show_minus(10, 1))
