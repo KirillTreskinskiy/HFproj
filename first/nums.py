f8a802b815a0db92223a8a8d67133d428fe46432
Kirill Treskinskiy <jdevrus@gmail.com>
2022-10-05 14:46:44 +0400

    add module strings.py del text

--- first/nums.py
+++ first/nums.py
@@ -2,7 +2,8 @@ def show_sum(a, b: int) -> int:
     return a + b
 
 
-text = 'some txt'
+def mul(a, b: int) -> int:
+    return a * b
 
 
 def show_minus(a, b: int) -> int:
@@ -12,3 +13,4 @@ def show_minus(a, b: int) -> int:
 if __name__ == '__main__':
     print(show_sum(2, 3))
     print(show_minus(10, 1))
+    print(mul(3, 3))
